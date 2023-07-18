ad5af61c20498c5f74cd38e986a60b579861d97c
usavk <113962032+usavk@users.noreply.github.com>
2023-07-18 20:31:50 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sum
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter a value");
+int a=s.nextInt();
+System.out.println("enter b value");
+int b=s.nextInt();
+System.out.println("sum is:"+(a+b));
+}
+}
+
+
