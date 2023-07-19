446f6b404744a2d530b9d726d8d6c66558bf1331
usavk <113962032+usavk@users.noreply.github.com>
2023-07-19 20:07:47 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class bitwise
+{
+public static void main(String[] args)
+{
+int x=5;
+int y=3;
+int a=x&y;
+int b=x|y;
+int c=~x;
+System.out.println("&="+a);
+System.out.println("|="+b);
+System.out.println("~="+c);
+}
+}
