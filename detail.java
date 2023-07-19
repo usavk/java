49801e4025429ab5e456017977a569b38c7239df
usavk <113962032+usavk@users.noreply.github.com>
2023-07-19 20:10:52 +0530

    Add files via upload

--- detail.java
+++ detail.java
@@ -8,12 +8,13 @@ System.out.println("enter the name:");
 String name=s.nextLine();
 System.out.println("enter the father name:");
 String fname=s.nextLine();
-System.out.println("enter the door no:");
-int dno=s.nextInt();
 System.out.println("enter the city:");
 String city=s.nextLine();
+System.out.println("enter the door no:");
+int dno=s.nextInt();
 System.out.println("enter the pincode:");
 int pincode=s.nextInt();
+
 System.out.println("name:"+name);
 System.out.println("father name:"+fname);
 System.out.println("door no:"+dno);
