81a11608dee75301139f7b6fed6108b1ac0ea4a7
usavk <113962032+usavk@users.noreply.github.com>
2023-07-19 20:05:04 +0530

    Add files via upload

---
+++ op.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class op
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter the values :");
+int a=s.nextInt();
+int b=s.nextInt();
+System.out.println("sum is:"+(a+b));
+System.out.println("difference is:"+(a-b));
+System.out.println("multiplicatin is:"+(a*b));
+System.out.println("division is:"+(a/b));
+System.out.println("remainder is:"+(a%b));
+}
+}
