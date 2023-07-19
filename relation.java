9c5cdba562a9dcd56cd19d7d60dd08c91cb45e1f
usavk <113962032+usavk@users.noreply.github.com>
2023-07-19 20:05:49 +0530

    Add files via upload

---
+++ relation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class relation
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter the two numbers:");
+int a=s.nextInt();
+int b=s.nextInt();
+System.out.println("a<b:"+(a<b));
+System.out.println("a<=b:"+(a<=b));
+System.out.println("a>b:"+(a>b));
+System.out.println("a>=b:"+(a>=b));
+System.out.println("a==b:"+(a==b));
+}
+}
